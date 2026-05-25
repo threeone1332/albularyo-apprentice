577caaec7269bde3a80b69973c82b98681e10d12
threeone1332 <angelarhian3101@gmail.com>
2026-05-25 21:10:36 +0800

    Add settings panel and reorganize main menu
    Rename scripts/start.gd -> scenes/main_menu.gd and switch the script to extend Control; add onready refs for the Panel and a on_settings_pressed handler, and hide the panel by default. Update main_menu.tscn to attach the new script, add UI textures and StyleBox subresources, rename the main VBoxContainer to MainButtons, and embed a Panel with settings UI (buttons, slider, AudioStreamPlayer). Add a new settings.tscn scene and include Scene_flow.pptx (diagram/flow).

--- game/scenes/main_menu.gd
+++ game/scenes/main_menu.gd
@@ -1,10 +1,15 @@
-extends Button
+extends Control
 
+@onready var panel: Panel = $Panel
+@onready var main_menu: Control = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	panel.visible = false;
 
+func on_settings_pressed():
+	panel.visible = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
